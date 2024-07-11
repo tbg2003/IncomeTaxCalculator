5ff0963a8c16abaa72f2b6c26c4bb73f427f597f
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-07-11 10:02:08 +0100

    working tax logic and tests, github pissing me off

--- src/main/scala/TaxCalculator.scala
+++ src/main/scala/TaxCalculator.scala
@@ -1,3 +1,6 @@
+
+import java.util.Locale
+
 class TaxCalculator {
 
   // Tax bands (simplified to make testing a bit easier)
@@ -13,18 +16,46 @@ class TaxCalculator {
 
   // A method to calculate the total amount of tax to be paid, returned as a double
   def calculateTax(income: Double): Double = {
-    ???
+    val basicTaxible = (basicRateLimit - personalAllowance)*basicRate
+    val higherTaxible = (higherRateLimit - basicRateLimit)*higherRate
+
+    if (income <= personalAllowance){
+      income * personalAllowanceRate
+    }
+    else if (income <= basicRateLimit){
+      (income-personalAllowance)*basicRate
+    }
+    else if (income <= higherRateLimit){
+      (income-basicRateLimit)*higherRate + basicTaxible
+    }
+    else if (income > higherRateLimit){
+      (income-higherRateLimit)*additionalRate + higherTaxible + basicTaxible
+    }
+    else{-1.0}
   }
+
 
   // A method which can tell you if someone is a higher rate taxpayer
   def isHigherRateTaxpayer(income: Double): Boolean = {
-    ???
+    income > basicRateLimit
   }
 
   // A method that will return a string with the income limit of their current tax band.
   // The return will also be formatted, E.g: "£12,500" or "No limit"
   def formattedCurrentTaxAllowance(income: Double): String = {
-    ???
+    val formatter = java.text.NumberFormat.getCurrencyInstance(Locale.UK)
+    if (income <= personalAllowance) {
+      formatter.format(personalAllowance)
+    } else if (income <= basicRateLimit) {
+      formatter.format(basicRateLimit)
+    } else if (income <= higherRateLimit) {
+      formatter.format(higherRateLimit)
+    } else if (income > higherRateLimit) {
+      "No Limit"
+    }
+    else {
+      "Error"
+    }
   }
 
 }
--- src/test/scala/TaxCalculatorSpec.scala
+++ src/test/scala/TaxCalculatorSpec.scala
@@ -9,10 +9,57 @@ class TaxCalculatorSpec extends AnyWordSpec {
   "TaxCalculator.calculateTax" should {
     "return the total amount of tax to pay" when {
       "the income is below the personal tax limit" in {
-        val result: Double = taxCalculator.calculateTax(5000)
-
-        assert(result == 0)
+        val result: Double = taxCalculator.calculateTax(9000)
+        assert(result == 0.0)
       }
+      "the income is below the basic rate limit" in {
+        val result: Double = taxCalculator.calculateTax(15_000)
+        assert(result == 1_000.0)
+      }
+      "the income is below the higher rate limit" in {
+        val result: Double = taxCalculator.calculateTax(60_000)
+        assert(result == 12_000.0)
+      }
+      "the income is above the higher rate limit" in {
+        val result: Double = taxCalculator.calculateTax(130_000)
+        assert(result == 40_250.0)
+      }
+    }
+  }
+  "TaxCalculator.isHigherRateTaxpayer" should {
+    "return true if someone is a higher rate taxpayer " when {
+      "the income is below the basic tax limit" in {
+        assert(!taxCalculator.isHigherRateTaxpayer(10000))
+      }
+      "the income is the basic tax limit" in {
+        assert(!taxCalculator.isHigherRateTaxpayer(50000))
+      }
+      "the income is above the higher rate limit" in {
+        assert(taxCalculator.isHigherRateTaxpayer(100000))
+      }
+    }
+  }
+  "TaxCalculator.formattedCurrentTaxAllowance" should {
+    "return a formatted string with the income limit of the current tax band" when {
+      "the income is below the personal allowance rate tax limit" in {
+        assert(taxCalculator.formattedCurrentTaxAllowance(5000.0) == "£10,000.00")
+      }
+      "the income is below the basic tax limit rate tax limit" in {
+        assert(taxCalculator.formattedCurrentTaxAllowance(11_000.0)=="£50,000.00")
+      }
+      "the income is the basic tax limit" in {
+        assert(taxCalculator.formattedCurrentTaxAllowance(50_000.0)=="£50,000.00")
+      }
+      "the income is above the basic rate limit" in {
+        assert(taxCalculator.formattedCurrentTaxAllowance(75_000.0)=="£125,000.00")
+      }
+      "the income is above the higher rate limit" in {
+        assert(taxCalculator.formattedCurrentTaxAllowance(150_000.0)=="No Limit")
+      }
+      "the income is negative" in {
+        assert(taxCalculator.formattedCurrentTaxAllowance(-100)=="£10,000.00")
+      }
+
     }
   }
 }
